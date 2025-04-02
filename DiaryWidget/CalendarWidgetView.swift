acb0ca396cc110b63790f5138bbcb6a8262ee69f
SeongHuiJung <tjdgml150237@naver.com>
2025-04-02 19:09:23 +0900

    [Feat] 위젯 추가 초기설정

---
+++ DiaryWidget/CalendarWidgetView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarWidgetView.swift
+//  TodayDiary
+//
+//  Created by 정성희 on 3/21/25.
+//
+
+import SwiftUI
+
+struct CalendarWidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarWidgetView()
+}
---
+++ DiaryWidget/WidgetData.swift
@@ -0,0 +1,8 @@
+//
+//  WidgetData.swift
+//  TodayDiary
+//
+//  Created by 정성희 on 3/31/25.
+//
+
+import Foundation
